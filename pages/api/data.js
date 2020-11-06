b2cbcacaffcb0d0c983cd8302d83d236024a80e4
aashishpeepra <ironhack082327@gmail.com>
2020-11-06 15:45:23 +0530

    Resolved static generation error in editor.js component

---
+++ pages/api/data.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {get_data} from "../../lib/data";
+export default (req, res) => {
+  console.log(req.query,req.method,req.body)
+  res.statusCode = 200
+  res.json(get_data());
+}
